bd095ffc4709a52bb359c692901366c4e1f4b0be
Yudi Prasetyo <prasetyaningyudi@gmail.cm>
2016-09-28 09:17:04 +0700

    + init_data.sql

---
+++ db/init_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO ROLE(NAMA_ROLE) VALUES
+('Administrator'),
+('Operator');
+
+INSERT INTO USER(USERNAME, PASSWORD, NAMA_USER, ROLE_ID) VALUES
+('administrator1', 'e19d5cd5af0378da05f63f891c7467af', 'Administrator Satu', '1'),
+('operator1', 'e19d5cd5af0378da05f63f891c7467af', 'Operator Satu', '2');
